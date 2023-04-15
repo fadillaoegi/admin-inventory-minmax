34810b96ccf213ac067dad3495eecbb5df6d2866
fadillaoegi <fadilfdl2710@gmail.com>
2023-04-16 03:27:22 +0700

    upadet from win

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main', function () {
     return view('main');
 });
 
-
+Route::get('/admin', [adminController::class, 'index'])->name('admin');
 
